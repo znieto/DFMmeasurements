e1969d9033795c8a19ffe6049a8962a0e1fd2282
zoraya <znieto@hotmail.com>
2018-01-19 08:47:10 +0100

    icon on new window and mpca-bk.png

--- DFMmeasurements/app/forms/gui_mp_meas_setup.py
+++ DFMmeasurements/app/forms/gui_mp_meas_setup.py
@@ -4,12 +4,19 @@ from PyQt5.Qt import QSize
 from app.core.CalibrationType import Calibration
 from PyQt5.QtWidgets import QDialog, QApplication
 import app.utils.guitools as guiutil
+import os
+import app.config.global_settings as g
 
 class gui_mp_meas_setup(QDialog):
     current_calibration =None
     def setupUi(self):
         width=1100
         height=730
+
+        #set Icon Window
+        pathimg = os.path.join(g.IMAGE_PATH, 'icon.png')
+        self.setWindowIcon(QtGui.QIcon(pathimg))
+
         self.setObjectName("Dialog")
         self.resize(width, 730)
         self.buttonBox = QtWidgets.QDialogButtonBox(self)
@@ -25,21 +32,14 @@ class gui_mp_meas_setup(QDialog):
             guiutil.addImageScaled(self.lblImage,"MpCal-AT.png",width-10,height-10)
         else:
             #image sjlfj
-            guiutil.addImage(self.lblImage,"MpCal-BK.emf")
-         
-        self.lblTitle = QtWidgets.QLabel(self)
-        self.lblTitle.setGeometry(QtCore.QRect(340, 0, 111, 20))
-        self.lblTitle.setObjectName("lblTitle")        
-
-        self.retranslateUi(self)
+            guiutil.addImageScaled(self.lblImage,"MpCal-BK.png",width-10,height-10)
+        
+               
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.close)
         QtCore.QMetaObject.connectSlotsByName(self)
 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.lblTitle.setText(_translate("Dialog", "Calibration Setup"))
+
 
     def __init__(self, parent= None, calibration= None):
         super().__init__()
